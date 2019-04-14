03e513477a2c78405b76b4f864a59e0b4eb1fe47
Kenton Archer <kenton8654@yahoo.com>
2019-04-14 12:53:00 -0400

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,59 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+require 'pry'
+def roll_call_dwarves(dwarf)
+ small_ppl = []
+  dwarf.each_with_index do |item, index|
+ small_ppl.push("#{index +1},#{item} ")
+  end
+
+   #binding.pry
+    puts small_ppl
+#binding.pry
+    end
+
+
+def summon_captain_planet(planeteer_calls)
+  hero = []
+  elements_array = planeteer_calls.collect do |calls|
+    calls.capitalize + "!"
+      end
+        return elements_array
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+  # have to capitalize each element and add exclamation point.
+  #return an array
+
+
+
+
+def long_planeteer_calls(shout)
+  shout.any? do |char|
+   if char.size > 4
+    true
+    else
+   false
+  end
+    end
 end
 
-def long_planeteer_calls# code an argument here
+  # code an argument here
   # Your code here
-end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+
+def find_the_cheese(strings)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  strings.find do |word|
+    binding.pry 
+    cheese_types.include?(word)
+  #   if word == cheese_types
+  #     puts cheese_types
+  #   else
+  #     return nil
+  # end
+    end
 end
+
+
+ # 1. should accept an array of strings
+ # 2. should look through these arrays and return first string that is a cheese_type
+ # 3. of cheese
+ # 4. if a list of ingredients !cheese return nil .
